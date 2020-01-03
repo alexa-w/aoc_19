fdf72b00621b41eb9aff883ea5a191d497c9c566
Alexa Welch <alexa.welch@ip-192-168-30-39.ec2.internal>
2020-01-03 16:19:20 -0600

    define Day5.add

--- lib/aoc19/day_5.ex
+++ lib/aoc19/day_5.ex
@@ -1,3 +1,9 @@
 defmodule Day5 do
+  @doc """
 
+  """
+  def add(sequence, [[first, second], verb], :position_mode) do
+    sum = Enum.fetch!(sequence, first) + Enum.fetch!(sequence, second)
+    List.replace_at(sequence, verb, sum)
+  end
 end
--- test/aoc19/day_5_test.exs
+++ test/aoc19/day_5_test.exs
@@ -1,11 +1,22 @@
 defmodule Day5Test do
-  use ExUnit.Case
-
-  setup_all do
-    {:ok, input: File.read!("bin/day_5")}
-  end
+  use ExUnit.Case, async: true
 
   test "runs", context do
-    assert context.input
+    assert {:ok}
   end
+
+  describe "output" do
+    @describetag :acceptance
+
+  end
+
+  describe "operations" do
+    @describetag :unit
+
+    test "add" do
+      assert Day5.add([1, 3, 4, 1, 99], [[3, 4], 1], :position_mode) == [1, 100, 4, 1, 99]
+    end
+
+  end
+
 end
