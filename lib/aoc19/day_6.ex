2a1affb0d34cd0da8d40bb03929454d188ae0038
Alexa Welch <alexa.welch@ip-192-168-30-16.ec2.internal>
2020-01-10 14:31:52 -0600

    marshals input

--- lib/aoc19/day_6.ex
+++ lib/aoc19/day_6.ex
@@ -1,4 +1,8 @@
 defmodule OrbitMap do
+  def marshal(input) do
+    String.split(input, "\n")
+  end
+
   def checksum(input) do
     0
   end
--- test/aoc19/day_6_test.exs
+++ test/aoc19/day_6_test.exs
@@ -4,6 +4,12 @@ defmodule OrbitMapTest do
   describe "acceptance" do
     @describetag :acceptance
 
+    test "marshals data" do
+      assert OrbitMap.marshal(
+        "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"
+      ) == ["COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L"]
+    end
+
     test "generates checksum" do
       assert OrbitMap.checksum(
         "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") == 42
